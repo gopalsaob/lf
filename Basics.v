3c86ec281cf90a7cfc5a688a36f978713a8d401b
gopal.shah <gopalshah.us@gmail.com>
2025-03-12 13:38:57 -0400

    Basics.v part4

--- Basics.v
+++ Basics.v
@@ -1160,7 +1160,11 @@ Proof.
 Theorem mult_n_1 : forall p : nat,
   p * 1 = p.
 Proof.
-  (* FILL IN HERE *) Admitted.
+  intros p.
+  rewrite <- mult_n_Sm.
+  rewrite <- mult_n_O.
+  reflexivity. Qed.
+  
 
 (** [] *)
 
